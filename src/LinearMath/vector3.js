60698aa7ecf6b456c1dcb6c1003fb61028d31928
Andrew Dolce <andrew@gradientstudios.com>
2011-12-14 15:58:29 -0500

    Added more methods and tests, Rotate and angle funcs dont agree with SIMD_EPSILON #3

--- src/LinearMath/vector3.js
+++ src/LinearMath/vector3.js
@@ -3,6 +3,8 @@
  */
 (function( window, Bump ) {
 
+  Bump.SIMD_EPSILON = Math.pow(2, -53);
+
   Bump.Vector3 = Bump.type( {
     typeMembers : {
       create : function( x, y, z ) {
@@ -11,6 +13,7 @@
         dest[0] = x || 0;
         dest[1] = y || 0;
         dest[2] = z || 0;
+        dest[3] = 0;
 
         return dest;
       },
@@ -47,8 +50,9 @@
         return dest;
       },
 
+      // scalar multiplication
       multiply : function( vec, scalar, dest ) {
-        if( !dest || vec == dest ){
+        if( !dest || vec == dest ) {
           vec[0] *= scalar;
           vec[1] *= scalar;
           vec[2] *= scalar;
@@ -63,6 +67,24 @@
         return dest;
       },
 
+      // element-wise vector multiplication
+      scale : function( vec, vec2, dest ) {
+        if( !dest || vec == dest ) {
+          vec[0] *= vec2[0];
+          vec[1] *= vec2[1];
+          vec[2] *= vec2[2];
+
+          return vec;
+        }
+
+        dest[0] = vec[0] * vec2[0];
+        dest[1] = vec[1] * vec2[1];
+        dest[2] = vec[2] * vec2[2];
+
+        return dest;
+      },
+
+      // scalar division
       divide : function( vec, scalar, dest ) {
         if( !dest || vec == dest ){
           vec[0] /= scalar;
@@ -79,17 +101,245 @@
         return dest;
       },
 
+      // element-wise vector division
+      // (not happy with the naming scheme, come up with something better)
+      divideElements : function( vec, vec2, dest ) {
+        if( !dest || vec == dest ){
+          vec[0] /= vec2[0];
+          vec[1] /= vec2[1];
+          vec[2] /= vec2[2];
+
+          return vec;
+        }
+
+        dest[0] = vec[0] / vec2[0];
+        dest[1] = vec[1] / vec2[1];
+        dest[2] = vec[2] / vec2[2];
+
+        return dest;
+      },
+
       dot : function( vec, vec2 ) {
         return vec[0] * vec2[0] + vec[1] * vec2[1] + vec[2] * vec2[2];
       },
 
       length2 : function( vec ) {
-        return Bump.Vector3.dot( vec, vec );
+        //return Bump.Vector3.dot( vec, vec );
+        return this.dot( vec, vec );
       },
 
       length : function( vec ) {
-        return Math.sqrt( Bump.Vector3.length2( vec, vec ) );
+        return Math.sqrt( this.length2( vec, vec ) );
+      },
+
+      distance2 : function( vec, vec2 ) {
+        return this.length2( this.subtract( vec2, vec, this.create() ) );
+      },
+
+      distance : function( vec, vec2 ) {
+        return this.length( this.subtract( vec2, vec, this.create() ) );
+      },
+
+      safeNormalize : function( vec ) {
+        var absVec = this.absolute( vec, this.create() ),
+        maxIndex = this.maxAxis( absVec );
+
+        if( absVec[ maxIndex ] > 0 ) {
+          this.divide( vec, absVec[ maxIndex ] );
+          return this.divide( vec, this.length( vec ) );
+        }
+
+        this.setValue( vec, 1, 0, 0 );
+        return vec;
+      },
+
+      normalize : function( vec ) {
+        return this.divide( vec, this.length( vec ) );
+      },
+
+      normalized : function( vec, dest ) {
+        dest = dest || this.create();
+        return this.divide( vec, this.length( vec ), dest );
+      },
+
+      /** Return a rotated version of vec
+       * param wAxis The axis to rotate about
+       * param angle The angle to rotate by */
+      rotate : function( vec, wAxis, angle, dest ) {
+        dest = dest || vec;
+        console.log('dest = ' + dest);
+
+        // wAxis must be a unit length vector
+        var o = this.multiply( wAxis, this.dot( wAxis, vec ), this.create() ),
+        x = this.subtract( vec, o, this.create() ),
+        y = this.cross( wAxis, vec, this.create() );
+
+        console.log('o = ' + o);
+        console.log('vec = ' + vec);
+
+        return this.add( this.add( o, this.multiply( x, Math.cos( angle ) ) ),
+                         this.multiply( y, Math.sin( angle ) ), dest );
+      },
+
+      // Return the angle between vec and vec2
+      angle : function( vec, vec2 ) {
+        var s = Math.sqrt( this.length2( vec ) * this.length2( vec2 ) );
+        // btFullAssert( s != btScalar( 0.0 ) )
+        return Math.acos( this.dot( vec, vec2 ) / s );
+      },
+
+      // Return a vector with the absolute values of each element
+      absolute : function( vec, dest ) {
+        if( !dest || vec == dest ) {
+          vec[0] = Math.abs( vec[0] );
+          vec[1] = Math.abs( vec[1] );
+          vec[2] = Math.abs( vec[2] );
+          return vec;
+        }
+
+        dest[0] = Math.abs( vec[0] );
+        dest[1] = Math.abs( vec[1] );
+        dest[2] = Math.abs( vec[2] );
+
+        return dest;
+      },
+
+      // Return the cross product between the two vectors, using dest if provided.
+      // Otherwise, the result overwrites vec
+      cross : function( vec, vec2, dest ) {
+        if( !dest ) {
+          dest = vec;
+        }
+        var x = vec[1] * vec2[2] - vec[2] * vec2[1],
+        y = vec[2] * vec2[0] - vec[0] * vec2[2],
+        z = vec[0] * vec2[1] - vec[1] * vec2[0];
+
+        dest[0] = x;
+        dest[1] = y;
+        dest[2] = z;
+
+        return dest;
+      },
+
+      // Return the triple cross product between the three vectors
+      triple : function( vec, vec2, vec3) {
+        return vec[0] * ( vec[1] * vec2[2] - vec[2] * vec2[1] ) +
+          vec[1] * ( vec[2] * vec2[0] - vec[0] * vec2[2] ) +
+          vec[2] * ( vec[0] * vec2[1] - vec[1] * vec2[0] );
+      },
+
+      // Return the axis with the smallest value
+      // Note: return values are 0, 1, 2 for x, y, or z
+      minAxis : function( vec ) {
+        return vec[0] < vec[1] ? ( vec[0] < vec[2] ? 0 : 2 ) : ( vec[1] < vec[2] ? 1 : 2 );
+      },
+
+      // Return the axis with the largest value
+      // Note: return values are 0, 1, 2 for x, y, or z
+      maxAxis : function( vec ) {
+        return vec[0] > vec[1] ? ( vec[0] > vec[2] ? 0 : 2 ) : ( vec[1] > vec[2] ? 1 : 2 );
+      },
+
+      // Note : the furthest and closest axis functions seem backwards...?
+      furthestAxis : function( vec ) {
+        return this.minAxis( this.absolute( vec, this.create() ) );
+      },
+
+      closestAxis : function( vec ) {
+        return this.maxAxis( this.absolute( vec, this.create() ) );
+      },
+
+      // Linearly interpolate between the two vectors, putting result in dest, or vec if dest is not specified
+      setInterpolate3 : function( vec, vec2, rt, dest ) {
+        if( !dest ){
+          dest = vec;
+        }
+
+        var s = 1 - rt;
+        dest[0] = s * vec[0] + rt * vec2[0];
+        dest[1] = s * vec[1] + rt * vec2[1];
+        dest[2] = s * vec[2] + rt * vec2[2];
+
+        return dest;
+      },
+
+      // not sure if this needs to exist as well as setInterpolate3...
+      lerp : function( vec, vec2, t, dest ) {
+        return this.setInterpolate3( vec, vec2, t, dest );
+      },
+
+      // not sure that get/set funcs make sense, so omitting the following:
+      // getX,
+      // getY,
+      // getZ,
+      // setX,
+      // setY,
+      // setZ,
+      // getW,
+      // x
+      // y
+      // z
+      // w
+
+      // element-wise comparison of vectors ( note : ignores w values )
+      equal : function( vec, vec2 ) {
+        return ( vec[0] == vec2[0] ) && ( vec[1] == vec2[1] ) && ( vec[0] == vec2[0] );
+      },
+
+      notEqual : function( vec, vec2 ) {
+        return ( vec[0] != vec2[0] ) || ( vec[1] != vec2[1] ) || ( vec[0] != vec2[0] );
+      },
+
+      // set vec's elements to be the max of its original elements and vec2's elements
+      setMax : function( vec, vec2 ) {
+        vec[0] = Math.max( vec[0], vec2[0] );
+        vec[1] = Math.max( vec[1], vec2[1] );
+        vec[2] = Math.max( vec[2], vec2[2] );
+        vec[3] = Math.max( vec[3], vec2[3] );
+
+        return vec;
+      },
+
+      // set vec's elements to be the min of its original elements and vec2's elements
+      setMin : function( vec, vec2 ) {
+        vec[0] = Math.min( vec[0], vec2[0] );
+        vec[1] = Math.min( vec[1], vec2[1] );
+        vec[2] = Math.min( vec[2], vec2[2] );
+        vec[3] = Math.min( vec[3], vec2[3] );
+
+        return vec;
+      },
+
+      // set vec's elements equal to x, y, z, w
+      setValue : function( vec, x, y, z ) {
+        vec[0] = x;
+        vec[1] = y;
+        vec[2] = z;
+        vec[3] = 0;
+
+        return vec;
+      },
+
+      // set the column(?) vectors v0, v1, v2 equal to the columns of the skew symmetric matrix
+      getSkewSymmetricMatrix : function( vec, v0, v1, v2 ) {
+        this.setValue( v0, 0, -vec[2], vec[1] );
+        this.setValue( v1, vec[2], 0, -vec[0] );
+        this.setValue( v2, -vec[1], vec[0], 0 );
+      },
+
+      setZero : function( vec ) {
+        this.setValue( vec, 0, 0, 0 );
+      },
+
+      isZero : function( vec ) {
+        return ( vec[0] === 0 ) && ( vec[1] === 0 ) && ( vec[2] === 0 );
+      },
+
+      fuzzyZero : function( vec ) {
+        return this.length2( vec ) < Bump.SIMD_EPSILON;
       }
+
+      // todo : serialization methods
     }
   } );
 } )( this, this.Bump );
--- tests/unit/vector3.js
+++ tests/unit/vector3.js
@@ -18,22 +18,26 @@ test( 'functions exist', 8, function() {
 module( 'Bump.Vector3.create' );
 
 test( 'create produces arrays', 2, function() {
-    deepEqual( Bump.Vector3.create(), [0, 0, 0] );
-    deepEqual( Bump.Vector3.create(1, 2, 3), [1, 2, 3] );
+  deepEqual( Bump.Vector3.create(), [ 0, 0, 0, 0 ] );
+  deepEqual( Bump.Vector3.create(1, 2, 3), [ 1, 2, 3, 0 ] );
 } );
 
 module( 'Bump.Vector3 math functions' );
 
-test( 'add function', 2, function() {
+test( 'add function', 5, function() {
   var v1 = Bump.Vector3.create(1, 2, 3),
   v2 = Bump.Vector3.create(1, -2, 0),
-  v3 = Bump.Vector3.create();
+  v3 = Bump.Vector3.create(),
+  ret;
 
-    Bump.Vector3.add(v1, v2, v3);
-    deepEqual(v3, [2, 0, 3], 'add with destination works');
+  ret = Bump.Vector3.add( v1, v2, v3 );
+  equal( ret, v3, 'with destination : return value reflects destination' );
+  deepEqual(v1, Bump.Vector3.create( 1, 2, 3 ), 'with destination : original vec unchanged' );
+  deepEqual(v3, Bump.Vector3.create( 2, 0, 3 ), 'with destination : correct result');
 
-    Bump.Vector3.add(v1, v2);
-    deepEqual(v1, [2, 0, 3], 'add in place works');
+  ret = Bump.Vector3.add(v1, v2);
+  equal( ret, v1, 'in place : return value correct');
+  deepEqual(v1, Bump.Vector3.create( 2, 0, 3 ), 'in place : correct result');
 } );
 
 test( 'subtract function', 2, function() {
@@ -41,11 +45,11 @@ test( 'subtract function', 2, function() {
   v2 = Bump.Vector3.create( 1, -2, 0 ),
   v3 = Bump.Vector3.create();
 
-    Bump.Vector3.subtract( v1, v2, v3 );
-    deepEqual( v3, [0, 4, 3], 'subtract with destination works');
+  Bump.Vector3.subtract( v1, v2, v3 );
+  deepEqual( v3, Bump.Vector3.create( 0, 4, 3 ), 'subtract with destination works');
 
-    Bump.Vector3.subtract( v1, v2 );
-    deepEqual( v1, [0, 4, 3], 'subtract in place works');
+  Bump.Vector3.subtract( v1, v2 );
+  deepEqual( v1, Bump.Vector3.create( 0, 4, 3 ), 'subtract in place works');
 } );
 
 test( 'multiply function', 2, function() {
@@ -53,10 +57,10 @@ test( 'multiply function', 2, function() {
   v2 = Bump.Vector3.create();
 
   Bump.Vector3.multiply( v1, 2, v2 );
-  deepEqual(v2, [2, 4, 6], 'multiply with destination works');
+  deepEqual(v2,  Bump.Vector3.create( 2, 4, 6 ), 'multiply with destination works');
 
   Bump.Vector3.multiply( v1, 2 );
-  deepEqual(v1, [2, 4, 6], 'multiply in place works');
+  deepEqual(v1, Bump.Vector3.create( 2, 4, 6 ), 'multiply in place works');
 } );
 
 test( 'divide function', 2, function() {
@@ -64,10 +68,10 @@ test( 'divide function', 2, function() {
   v2 = Bump.Vector3.create();
 
   Bump.Vector3.divide( v1, 2, v2 );
-  deepEqual(v2, [0.5, 1, 1.5], 'divide with destination works');
+  deepEqual(v2,  Bump.Vector3.create( 0.5, 1, 1.5 ), 'divide with destination works');
 
   Bump.Vector3.divide( v1, 2 );
-  deepEqual(v1, [0.5, 1, 1.5], 'divide in place works');
+  deepEqual(v1, Bump.Vector3.create( 0.5, 1, 1.5 ), 'divide in place works');
 } );
 
 test( 'dot function', 1, function() {
@@ -84,4 +88,107 @@ test( 'length2 function', 1, function() {
 test( 'length function', 1, function() {
   var v1 = Bump.Vector3.create( 1, 2, 3 );
   equal( Bump.Vector3.length( v1 ), Math.sqrt( 14 ) );
+} );
+
+test( 'distance2 function', 1, function() {
+  var v1 = Bump.Vector3.create( 2, 2, 2 ),
+  v2 = Bump.Vector3.create( -1, -1, -1 );
+  equal( Bump.Vector3.distance2( v1, v2 ), 27 );
+} );
+
+test( 'distance function', 1, function() {
+  var v1 = Bump.Vector3.create( 2, 2, 2 ),
+  v2 = Bump.Vector3.create( -1, -1, -1 );
+  equal( Bump.Vector3.distance( v1, v2 ), Math.sqrt( 27 ) );
+} );
+
+test( 'safeNormalize', 3, function() {
+  var v1 = Bump.Vector3.create( 2, 0, 0 ),
+  v2 = Bump.Vector3.create( 0, 1, -1 ),
+  v3 = Bump.Vector3.create();
+
+  deepEqual( Bump.Vector3.safeNormalize( v1 ), Bump.Vector3.create( 1, 0, 0 ) );
+  deepEqual( Bump.Vector3.safeNormalize( v2 ), Bump.Vector3.create( 0, 1/Math.sqrt( 2 ), -1/Math.sqrt( 2 ) ) );
+  deepEqual( Bump.Vector3.safeNormalize( v3 ), Bump.Vector3.create( 1, 0, 0 ) );
+} );
+
+test( 'normalize', 2, function() {
+  var v1 = Bump.Vector3.create( 2, 0, 0 ),
+  v2 = Bump.Vector3.create( 0, 1, -1 );
+
+  deepEqual( Bump.Vector3.normalize( v1 ), Bump.Vector3.create( 1, 0, 0 ) );
+  deepEqual( Bump.Vector3.normalize( v2 ), Bump.Vector3.create( 0, 1/Math.sqrt( 2 ), -1/Math.sqrt( 2 ) ) );
+} );
+
+test( 'normalized', 4, function() {
+  var v1 = Bump.Vector3.create( 2, 0, 0 ),
+  v2 = Bump.Vector3.create( 0, 1, -1 ),
+  v3 = Bump.Vector3.create(),
+  v4,
+  n1 = Bump.Vector3.create( 1, 0, 0 ),
+  n2 = Bump.Vector3.create( 0, 1/Math.sqrt( 2 ), -1/Math.sqrt( 2 ) );
+
+  Bump.Vector3.normalized( v1, v3 );
+  v4 = Bump.Vector3.normalized( v2, Bump.Vector3.create() );
+
+  deepEqual( v1, Bump.Vector3.create( 2, 0, 0 ) );
+  deepEqual( v2, Bump.Vector3.create( 0, 1, -1 ) );
+  deepEqual( v3, n1 );
+  deepEqual( v4, n2 );
+} );
+
+// definitely should add more tests for this one
+test( 'rotate', 9, function() {
+  var v1 = Bump.Vector3.create( 1, 0, 0 ),
+  zAxis = Bump.Vector3.create( 0, 0, 1 ),
+  vRot = Bump.Vector3.create(),
+  ret;
+
+  ret = Bump.Vector3.rotate( v1, zAxis, Math.PI/2, vRot );
+
+  equal( ret, vRot );
+  deepEqual( v1, Bump.Vector3.create( 1, 0, 0 ) );
+  ok( Math.abs( vRot[0] ) < Bump.SIMD_EPSILON, 'vRot[0] is close to 0' );
+  ok( Math.abs( vRot[1] - 1 ) < Bump.SIMD_EPSILON, 'vRot[1] is close to 1' );
+  ok( Math.abs( vRot[0] ) < Bump.SIMD_EPSILON, 'vRot[2] is close to 0' );
+
+  ret = Bump.Vector3.rotate( v1, zAxis, Math.PI );
+
+  equal( ret, v1 );
+  ok( Math.abs( v1[0] + 1 ) < Bump.SIMD_EPSILON, 'v1[0] is close to -1' );
+  ok( Math.abs( v1[1] ) < Bump.SIMD_EPSILON, 'v1[1] is close to 0' );
+  ok( Math.abs( v1[2] ) < Bump.SIMD_EPSILON, 'v1[2] is close to 0' );
+} );
+
+test( 'angle', 7, function() {
+  var right = Bump.Vector3.create( 1, 0, 0 ),
+  up = Bump.Vector3.create( 0, 1, 0 ),
+  left = Bump.Vector3.create( -1, 0, 0 ),
+  forward = Bump.Vector3.create( 0, 0, 1 ),
+  upRight = Bump.Vector3.create( 1, 1, 0 );
+
+  ok( Math.abs( Bump.Vector3.angle( right, up ) - Math.PI / 2 ) < Bump.SIMD_EPSILON );
+  ok( Math.abs( Bump.Vector3.angle( right, forward ) - Math.PI / 2 ) < Bump.SIMD_EPSILON );
+  ok( Math.abs( Bump.Vector3.angle( right, left ) - Math.PI ) < Bump.SIMD_EPSILON );
+  ok( Math.abs( Bump.Vector3.angle( right, upRight ) - Math.PI / 4 ) < Bump.SIMD_EPSILON );
+  ok( Math.abs( Bump.Vector3.angle( left, upRight ) - 3 * Math.PI / 4 ) < Bump.SIMD_EPSILON );
+  ok( Math.abs( Bump.Vector3.angle( up, upRight ) - Math.PI / 4 ) < Bump.SIMD_EPSILON );
+  ok( Math.abs( Bump.Vector3.angle( forward, upRight ) - Math.PI / 2 ) < Bump.SIMD_EPSILON );
+} );
+
+test( 'absolute', 5, function() {
+  var v1 = Bump.Vector3.create( -1, -2, 3 ),
+  v2 = Bump.Vector3.create(),
+  ret;
+
+  ret = Bump.Vector3.absolute( v1, v2 );
+
+  equal( ret, v2 );
+  deepEqual( v1, Bump.Vector3.create( -1, -2, 3 ) );
+  deepEqual( v2, Bump.Vector3.create( 1, 2, 3 ) );
+
+  ret = Bump.Vector3.absolute( v1 );
+
+  equal( ret, v1 );
+  deepEqual( v1, Bump.Vector3.create( 1, 2, 3 ) );
 } );
